a84872d83d083e5409eb70e06bef8966483c07dd
Aman Toshniwal <aman9999toshniwal@gmail.com>
2022-01-08 15:58:23 +0530

    Initial Commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Aman Toshniwal on 29/12/21.
+//  Copyright © 2021 Aman Toshniwal. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String
+    let body: String
+}
